1b9e391f83c33d49fc7d368ec8a7dc00a6cb5403
jeesun <luckymeday@gmail.com>
2020-07-27 16:06:22 +0700

    movie card3

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -20,21 +20,22 @@ export default function MovieCard(props) {
     }
     if (true) {
         return (
-            <div className="col-md-3">
-                <div className="card moviecard container" onMouseOver={showInfo} onMouseLeave={hideInfo}>
-                    <img src={fullImg} className="card-img-top" alt="..." />
-                    <div className="detail-info"><h5 className="card-title card-letter">{props.title}</h5>
-                        <div className="small-letter">
-                            <h6 className="card-text card-letter">{props.releaseDate}</h6>
-                            <h6 className="card-text card-letter">   ★ {props.rating}</h6>
-                        </div>
-                        <h6 className="card-text card-letter">{props.genre.map((genre) => { return (<Badge variant="dark" style={{ marginRight: "10px" }}>{props.genres.find((item) => item.id === genre).name}</Badge>) })}</h6>
-                        <hr></hr>
-                        {props.overview}
+            <div>hahaha{props.overview}</div>
+            // <div className="col-md-3">
+            //     <div className="card moviecard container" onMouseOver={showInfo} onMouseLeave={hideInfo}>
+            //         <img src={fullImg} className="card-img-top" alt="..." />
+            //         <div className="detail-info"><h5 className="card-title card-letter">{props.title}</h5>
+            //             <div className="small-letter">
+            //                 <h6 className="card-text card-letter">{props.releaseDate}</h6>
+            //                 <h6 className="card-text card-letter">   ★ {props.rating}</h6>
+            //             </div>
+            //             <h6 className="card-text card-letter">{props.genre.map((genre) => { return (<Badge variant="dark" style={{ marginRight: "10px" }}>{props.genres.find((item) => item.id === genre).name}</Badge>) })}</h6>
+            //             <hr></hr>
+            //             {props.overview}
 
-                    </div>
-                </div>
-            </div>
+            //         </div>
+            //     </div>
+            // </div>
         )
     } else {
         return (
