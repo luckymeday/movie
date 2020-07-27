948e06afbcc07c9eee1af95291a4e666072dbad7
jeesun <luckymeday@gmail.com>
2020-07-27 16:01:58 +0700

    movie card2

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -18,20 +18,20 @@ export default function MovieCard(props) {
     const hideInfo = () => {
         setIsHover(false)
     }
-    if (isHover) {
+    if (true) {
         return (
             <div className="col-md-3">
                 <div className="card moviecard container" onMouseOver={showInfo} onMouseLeave={hideInfo}>
                     <img src={fullImg} className="card-img-top" alt="..." />
-                    <div className="detail-info"><h5 class="card-title card-letter">{props.title}</h5>
-                        <div class="small-letter">
-                            <h6 class="card-text card-letter">{props.releaseDate}</h6>
-                            <h6 class="card-text card-letter">   ★ {props.rating}</h6>
+                    <div className="detail-info"><h5 className="card-title card-letter">{props.title}</h5>
+                        <div className="small-letter">
+                            <h6 className="card-text card-letter">{props.releaseDate}</h6>
+                            <h6 className="card-text card-letter">   ★ {props.rating}</h6>
                         </div>
-                        <h6 class="card-text card-letter">{props.genre.map((genre) => { return (<Badge variant="dark" style={{ marginRight: "10px" }}>{props.genres.find((item) => item.id === genre).name}</Badge>) })}</h6>
+                        <h6 className="card-text card-letter">{props.genre.map((genre) => { return (<Badge variant="dark" style={{ marginRight: "10px" }}>{props.genres.find((item) => item.id === genre).name}</Badge>) })}</h6>
                         <hr></hr>
                         {props.overview}
-                        
+
                     </div>
                 </div>
             </div>
@@ -42,12 +42,12 @@ export default function MovieCard(props) {
                 <div className="card moviecard" onMouseOver={showInfo} onMouseLeave={hideInfo}>
                     <img src={fullImg} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 class="card-title card-letter">{props.title}</h5>
-                        <div class="small-letter">
-                            <h6 class="card-text card-letter">{props.releaseDate}</h6>
-                            <h6 class="card-text card-letter">   ★ {props.rating}</h6>
+                        <h5 className="card-title card-letter">{props.title}</h5>
+                        <div className="small-letter">
+                            <h6 className="card-text card-letter">{props.releaseDate}</h6>
+                            <h6 className="card-text card-letter">   ★ {props.rating}</h6>
                         </div>
-                        <h6 class="card-text card-letter">
+                        <h6 className="card-text card-letter">
                             {props.genre.map((genre) => {
                                 return (<Badge variant="dark" style={{ marginRight: "10px" }}>{props.genres.find((item) => item.id === genre).name}</Badge>)
                             })}</h6>
